136761ce15f639fb1569492f76adef7a0273538a
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-21 17:17:41 +0700

    chk hasNextPage

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -12,6 +12,7 @@
     <router-link
       :to="{ name: 'EventList', query: { page: page + 1} }"
       rel="next"
+      v-if="hasNextPage"
     >
       Next Page
     </router-link>
@@ -37,7 +38,8 @@ export default {
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0
     }
   },
   created() {
@@ -45,11 +47,18 @@ export default {
       EventService.getEvents(2, this.page)
       .then((response) => {
         this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
       })
       .catch((error) => {
         console.log(error)
       })
     })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    }
   }
 }
 </script>
