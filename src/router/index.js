15efa02664e155dcf0e52d6cb198b3af9da1b528
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-21 18:52:03 +0700

    NotFound page #1

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import EventDetails from '@/views/event/Details.vue'
 import EventRegister from '@/views/event/Register.vue'
 import EventEdit from '@/views/event/Edit.vue'
 import EventLayout from '@/views/event/Layout.vue'
+import NotFound from '@/views/NotFound.vue'
 const routes = [
   {
     path: '/',
@@ -39,6 +40,12 @@ const routes = [
         component: EventEdit
       }
     ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+    props: (route) => ({ page: parseInt(route.query.page) || 1})
   },
 ]
 
