e4267026ea829a77c0fb234bd1d32cabfdda882b
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-21 19:13:30 +0700

    NotFound page #2

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -32,6 +32,7 @@ export default {
         })
         .catch((error) => {
             console.log(error)
+            this.$router.push({name: '404Resource', params:{resource: 'event'}})
         })
     }
 }
