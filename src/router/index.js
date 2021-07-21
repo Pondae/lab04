005e6ae91698056427d4ce6200c19db57c072b2e
Pondae <phonmongkhon_ph@cmu.ac.th>
2021-07-21 18:46:04 +0700

    add the parent of other views

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import About from '../views/About.vue'
 import EventDetails from '@/views/event/Details.vue'
 import EventRegister from '@/views/event/Register.vue'
 import EventEdit from '@/views/event/Edit.vue'
+import EventLayout from '@/views/event/Layout.vue'
 const routes = [
   {
     path: '/',
@@ -18,22 +19,27 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
-    component: EventDetails,
-    props: true
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
   },
-  {
-    path: '/event/:id/register',
-    name: 'EventRegister',
-    component: EventRegister,
-    props: true
-  },
-  {
-    path: '/event/:id/edit',
-    name: 'EventEdit',
-    component: EventEdit,
-    props: true
-  }
 ]
 
 const router = createRouter({
